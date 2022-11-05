8e78645d6aaaefdbf4335e092d2adea40c819eb2
Riskusy <81322184+Riskusy@users.noreply.github.com>
2022-11-05 11:39:52 +0100

    First

---
+++ 31.js
@@ -0,0 +1,4 @@
+// Change code below this line
+const someAdjective = "hi there";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
---
+++ 48.js
@@ -0,0 +1,7 @@
+const myList = [
+    ["Chocolate Bar", 15],
+    ["Chocolate My", 15],
+    ["Chocolate So", 15],
+    ["Chocolate Fo", 15],
+    ["Chocolate Fi", 15],
+];
