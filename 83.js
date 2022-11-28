3843e3f2c27418c3bbddff234bad534337adcf2a
Riskusy <81322184+Riskusy@users.noreply.github.com>
2022-11-28 13:00:59 +0100

    Add files via upload

---
+++ 83.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+    name: "Hasky",
+    legs: 4,
+    tails: 1,
+    friends: ["human", "dog"]
+
+
+    // Only change code above this line
+};
